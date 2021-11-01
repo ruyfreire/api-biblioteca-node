b6110dbd31f0803fa2bd6775be67a8ad550c03a8
Ruy Freire <ruy.slv@gmail.com>
2021-11-01 18:18:55 -0300

    prisma config

---
+++ src/prisma/index.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '.prisma/client'
+
+const prismaClient = new PrismaClient({
+  errorFormat: 'pretty',
+  log: ['query', 'info', 'warn', 'error']
+})
+
+export { prismaClient }
