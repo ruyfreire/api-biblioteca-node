2991fb99ed5e8e1e272af65df979cbac3e1f5779
Ruy Freire <ruy.slv@gmail.com>
2021-11-01 18:40:46 -0300

    handler error prisma

---
+++ src/utils/HandlerErrorsPrisma/index.ts
@@ -0,0 +1,32 @@
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError
+} from '@prisma/client/runtime'
+
+export const handlerErrorsPrisma = (error: any) => {
+  if (error instanceof PrismaClientKnownRequestError) {
+    if (error.code === 'P2002') {
+      return {
+        code: 'error',
+        message: 'Autor já existe',
+        data: error.meta
+      }
+    }
+
+    return {
+      error: 'error',
+      message: 'Erro para criar novo autor',
+      data: error.message
+    }
+  }
+
+  if (error instanceof PrismaClientValidationError) {
+    return {
+      code: 'error',
+      message: 'Erro de validação dos campos enviados',
+      data: error.message
+    }
+  }
+
+  return ''
+}
