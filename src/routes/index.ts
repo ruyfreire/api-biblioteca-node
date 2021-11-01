21bf67196e57f76cd29f161fd0deaf45bd0cac05
Ruy Freire <ruy.slv@gmail.com>
2021-11-01 18:42:15 -0300

    route create author

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import { createAuthorController } from '../controllers/AuthorController'
 import { HomeController } from '../controllers/HomeController'
 
 const Routes = Router()
 
 Routes.get('/', HomeController)
+
+Routes.post('/author', createAuthorController)
 
 export { Routes }
