142b023085a6ad4caeb9e34d8cd2f46e1a9d3328
Ruy Freire <ruy.freire@lendico.com.br>
2021-11-04 19:10:05 -0300

    global messages yup

--- src/utils/Validators/globalSchema.ts
+++ src/utils/Validators/globalSchema.ts
@@ -8,6 +8,12 @@ setLocale({
   },
   object: {
     noUnknown: 'Campos desconhecidos: [${unknown}]'
+  },
+  string: {
+    min: 'O campo [${path}] precisa ter no mínimo ${min} caracteres',
+    max: 'O campo [${path}] precisa ter no máximo ${max} caracteres',
+    email: 'O campo [${path}] precisa ser um email válido',
+    url: 'O campo [${path}] precisa ser uma URL válida'
   }
 })
 
