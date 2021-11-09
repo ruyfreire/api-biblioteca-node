9523532d41d92e44dd640824cf3e31c7cd9bec60
Ruy Freire <ruy.slv@gmail.com>
2021-11-09 00:07:56 -0300

    code validate error yup

--- src/utils/Validators/handlerValidation.ts
+++ src/utils/Validators/handlerValidation.ts
@@ -3,6 +3,7 @@ import { ValidationError } from 'yup'
 export const handlerValidationError = (error: any) => {
   if (error instanceof ValidationError) {
     return {
+      code: 'error',
       message: 'Erro de validação dos campos',
       data: error.errors
     }
