9c801e99a3312b8f4dbdfd51e21d2da25c0d33b6
Ruy Freire <ruy.freire@lendico.com.br>
2022-01-07 14:53:38 -0300

    controller get all

---
+++ src/controllers/BookController/GetAllBookController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express'
+import { BookService } from '../../services/BookService'
+
+export class GetAllBookController {
+  constructor(private service: BookService) {}
+
+  async execute(req: Request, res: Response): Promise<Response> {
+    try {
+      const books = await this.service.getAll()
+
+      return res.status(200).json({
+        code: 'success',
+        message: 'Listagem com todos os livros',
+        data: books
+      })
+    } catch (error: Error | any) {
+      if (error.status) {
+        return res.status(error.status).json({
+          code: error.code,
+          message: error.message,
+          data: error.data || ''
+        })
+      }
+
+      return res.status(500).json({
+        code: 'error',
+        message: error.message
+      })
+    }
+  }
+}
