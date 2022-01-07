9d3161a19ab8bc91ad29815342515ab42c699622
Ruy Freire <ruy.freire@lendico.com.br>
2022-01-07 14:54:12 -0300

    controller delete

---
+++ src/controllers/BookController/DeleteBookController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+import { BookService } from '../../services/BookService'
+
+export class DeleteBookController {
+  constructor(private service: BookService) {}
+
+  async execute(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+
+      if (!Number(id)) {
+        return res.status(400).json({
+          code: 'error',
+          message: 'ID inválido'
+        })
+      }
+
+      const deletedBook = await this.service.delete(Number(id))
+
+      return res.status(200).json({
+        code: 'success',
+        message: 'Livro deletado com sucesso',
+        book: deletedBook
+      })
+    } catch (error: Error | any) {
+      if (error.status) {
+        return res.status(error.status).json({
+          code: error.code,
+          message: error.message,
+          data: error.data || ''
+        })
+      }
+
+      return res.status(500).json({
+        code: 'error',
+        message: error.message
+      })
+    }
+  }
+}
