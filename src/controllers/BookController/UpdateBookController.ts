bcb1ebf121c6dd60b39f9552abf3935e79510c8b
Ruy Freire <ruy.freire@lendico.com.br>
2022-01-07 14:53:59 -0300

    controller update

---
+++ src/controllers/BookController/UpdateBookController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express'
+import { schemaCreateBook } from '../../utils/Validators/bookValidator'
+import { BookService, ICreateBook } from '../../services/BookService'
+import { handlerValidationError } from '../../utils/Validators/handlerValidation'
+
+export class UpdateBookController {
+  constructor(private service: BookService) {}
+
+  async execute(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+      const book = req.body as ICreateBook
+
+      if (!Number(id)) {
+        return res.status(400).json({
+          code: 'error',
+          message: 'ID inválido'
+        })
+      }
+
+      await schemaCreateBook.validate(book)
+
+      const updatedBook = await this.service.update(Number(id), book)
+
+      return res.status(201).json({
+        code: 'success',
+        message: 'Livro atualizado com sucesso',
+        data: updatedBook
+      })
+    } catch (error: Error | any) {
+      const validationError = handlerValidationError(error)
+
+      if (validationError) {
+        return res.status(400).json(validationError)
+      }
+
+      if (error.status) {
+        return res.status(error.status).json({
+          code: error.code,
+          message: error.message,
+          data: error.data || ''
+        })
+      }
+
+      return res.status(500).json({
+        code: 'error',
+        message: error.message
+      })
+    }
+  }
+}
