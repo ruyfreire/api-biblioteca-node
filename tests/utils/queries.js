1002d1a60aacbcd9c562ee8a7b8558b207ca958b
Ruy Freire <ruy.slv@gmail.com>
2022-02-21 22:52:01 -0300

    prisma atualização de tabela e migration

--- tests/utils/queries.js
+++ tests/utils/queries.js
@@ -15,13 +15,13 @@ export const createTableQueries = [
         bookId INTEGER NOT NULL,
     
         PRIMARY KEY ("authorId", "bookId"),
-        CONSTRAINT "author_book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "authors" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-        CONSTRAINT "author_book_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "books" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+        CONSTRAINT "author_book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "authors" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+        CONSTRAINT "author_book_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "books" ("id") ON DELETE CASCADE ON UPDATE CASCADE
     )`
 ]
 
 export const dropTableQueries = [
-  Prisma.sql`DROP TABLE main.author_book`,
-  Prisma.sql`DROP TABLE main.books`,
-  Prisma.sql`DROP TABLE main.authors`
+  Prisma.sql`DROP TABLE IF EXISTS main.author_book`,
+  Prisma.sql`DROP TABLE IF EXISTS main.books`,
+  Prisma.sql`DROP TABLE IF EXISTS main.authors`
 ]
