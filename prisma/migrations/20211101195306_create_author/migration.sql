d46ceaaef89d5e6652919feb99adbbb44f912312
Ruy Freire <ruy.slv@gmail.com>
2022-07-02 04:03:37 -0300

    alterado o banco de dados

--- prisma/migrations/20211101195306_create_author/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Author" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Author_name_key" ON "Author"("name");
